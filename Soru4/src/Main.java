916d0c734089bfd9eaf49999be72f779905842d0
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2023-03-15 18:27:31 +0300

    Update Main.java

--- Soru4/src/Main.java
+++ Soru4/src/Main.java
@@ -2,29 +2,29 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = new int[10000];
 // Rastgele dizi oluştur
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * 10000);
+        for (int x = 0; x < arr.length; x++) {
+            arr[x] = (int) (Math.random() * 10000);
         }
 
 // Quick sort'u çalıştır ve süreyi ölç
-        long startTime = System.nanoTime();
+        long start = System.nanoTime();
         quickSort(arr, 0, arr.length - 1);
-        long endTime = System.nanoTime();
-        System.out.println("Quick sort süresi: " + (endTime - startTime) + " nanosaniye");
+        long end = System.nanoTime();
+        System.out.println("Quick sort süresi: " + (end - start) + " nanosaniye");
 
 // Brute force'u çalıştır ve süreyi ölç
-        startTime = System.nanoTime();
+        start = System.nanoTime();
         bruteForceSort(arr);
-        endTime = System.nanoTime();
-        System.out.println("Brute force süresi: " + (endTime - startTime) + " nanosaniye");
+        end = System.nanoTime();
+        System.out.println("Brute force süresi: " + (end - start) + " nanosaniye");
     }
     public static void bruteForceSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+        for (int x = 0; x < arr.length - 1; x++) {
+            for (int y = x + 1; y < arr.length; y++) {
+                if (arr[y] < arr[x]) {
+                    int temp = arr[x];
+                    arr[x] = arr[y];
+                    arr[y] = temp;
                 }
             }
         }
@@ -39,18 +39,18 @@ public class Main {
 
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        int x = low - 1;
+        for (int y = low; y < high; y++) {
+            if (arr[y] < pivot) {
+                x++;
+                int temp = arr[x];
+                arr[x] = arr[y];
+                arr[y] = temp;
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
+        int temp = arr[x + 1];
+        arr[x + 1] = arr[high];
         arr[high] = temp;
-        return i + 1;
+        return x + 1;
     }
 }
